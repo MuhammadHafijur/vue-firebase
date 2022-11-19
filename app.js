1265b86dc46e01dbd3bd874c721f9e5e1c74bb35
MuhammadHafijur <hafijur.salah@gmail.com>
2022-11-19 19:08:09 +0600

    :sparkles: feat: conditional rendering

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
     data(){
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
         }
     },
     methods: {
-        changeTitle(title){
-            // this.title = 'words of randience'
-            this.title = title
+        toggleShowBooks(){
+            this.showBooks = !this.showBooks
         }
     }
 })
